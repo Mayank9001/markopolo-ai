b364a2f1176dbdc31cad137a18ad5033e44d88d2
Mayank Vishwakarma <mayankvishw09@gmail.com>
2025-05-04 20:12:14 +0530

    `Added GitHub link to the bottom right of the page and removed duplicate font import`

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,18 @@ const App = () => {
     <div className="p-4">
       <SearchBar onSearch={setSearch} />
       <UserTable search={search} />
+      <a
+        href="https://github.com/Mayank9001"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-4 right-4"
+      >
+        <img
+          src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+          alt="GitHub"
+          className="w-8 h-8 hover:opacity-60 transition"
+        />
+      </a>
     </div>
   );
 };
